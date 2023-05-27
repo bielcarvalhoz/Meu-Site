3e7bd6d2bccdb2ee7f508ea0daf5937c8ed7ff04
Gabriel <gabriel12f34@gmail.com>
2023-05-26 21:42:45 -0300

    ajustando venv e pegando feriados brasileros

--- meusite/home/urls.py
+++ meusite/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.TemplateHomeView.as_view(), name='home'),
+    # path('teste', views.CorporateHolidays, name='corporateHolidays'),
 ]
--- meusite/home/views.py
+++ meusite/home/views.py
@@ -1,6 +1,38 @@
+import holidays
+import pandas as pd
+from django.views.generic import View, TemplateView, CreateView, ListView
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime, date, time
+
+
 # Create your views here.
 
-def home(request):
-    return render(request, 'home/home.html')
+now = datetime.today()
+year = now.year
+
+FeriadosDiversos = [
+        (date(year, 6, 24), 'Dia de São João (Festa Junina)'),
+        (date(year, 6, 12), 'Dia dos Namorados'),
+        (date(year, 6, 12), 'Dia dos Namorados')
+    ]
+
+FeriadosDoBrasil = list(holidays.BR(subdiv='SP', years=year).items())
+FeriadosDoBrasil = FeriadosDoBrasil + FeriadosDiversos
+Feriados = []
+
+for date, name in sorted(FeriadosDoBrasil):
+    Feriados.append(f"{format(date, '%d/%m/%Y')} : {name}")  
+
+
+
+
+print(FeriadosDoBrasil)
+
+
+class TemplateHomeView(TemplateView):
+    template_name = 'home/home.html'
+    def get_context_data(self,**kwargs):
+        context = super ().get_context_data  (**kwargs)
+        context['feriados'] = Feriados
+        return context
